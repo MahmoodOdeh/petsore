2a924fd090623f5c042c49ac8800bb2ced535c49
MahmoodOdeh <odeh.mahmood3@gmail.com>
2024-03-23 13:35:11 +0200

    Update config_handler.py

--- infra/config_handler.py
+++ infra/config_handler.py
@@ -1,8 +1,10 @@
 import json
+import os
 
 
 class ConfigHandler:
     def __init__(self, config_file_path):
+        self.config_file_path = os.getenv('C:/Users/odehm/Desktop/repos/petsore/config.json')
         self.config = self.load_config(config_file_path)
 
     def load_config(self, config_file_path):
