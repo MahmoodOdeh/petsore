0e0dad65dd96e24af3af9fc60cee3a2b61e85b02
MahmoodOdeh <odeh.mahmood3@gmail.com>
2024-03-23 13:57:50 +0200

    Update browser_wrapper.py

--- infra/browser_wrapper.py
+++ infra/browser_wrapper.py
@@ -5,7 +5,7 @@ from infra.config_handler import ConfigHandler
 class BrowserWrapper:
     def __init__(self):
         self.driver = None
-        config_file_path = 'C:/Users/odehm/Desktop/seleniumGrid/PetStore/config.json'
+        config_file_path = 'config.json'
         self.config_handler = ConfigHandler(config_file_path)
         self.grid_enabled = self.config_handler.get_config_value('grid')
         self.serial_enabled = self.config_handler.get_config_value('serial')
