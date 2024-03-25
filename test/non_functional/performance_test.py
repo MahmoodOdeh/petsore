4c940750156d193c116ac052dbb25a0693e48f5e
MahmoodOdeh <odeh.mahmood3@gmail.com>
2024-03-26 00:16:13 +0200

    Update performance_test.py

--- test/non_functional/performance_test.py
+++ test/non_functional/performance_test.py
@@ -1,11 +1,12 @@
+import sys
+
+sys.path.append('C:/Users/odehm/Desktop/repos/petsore')
 import concurrent.futures
 import time
 import unittest
 from infra.browser_wrapper import BrowserWrapper
 from logic.logic_ui.search_page import PetStoreSearchPage
 from logic.logic_ui.home_page import PetStorePage
-import sys
-sys.path.append('C:/Users/odehm/Desktop/repos/petsore')
 
 
 class PerformanceTest(unittest.TestCase):
@@ -26,7 +27,7 @@ class PerformanceTest(unittest.TestCase):
         print(f"Page loaded in: {response_time:.2f} seconds")
         assert response_time < 5, "Page load time exceeded 5 seconds"
 
-    def test_assert_response_time_search(self,browser_type=None):
+    def test_assert_response_time_search(self, browser_type=None):
         driver = self.browser.get_driver(browser_type)
         driver.get(self.browser.get_driver_url())
         self.petstore = PetStorePage(driver)
@@ -39,3 +40,17 @@ class PerformanceTest(unittest.TestCase):
         response_time = end_time - start_time
         print(f"Search operation completed in: {response_time:.2f} seconds")
         assert response_time < 5, "Search operation took more than 5 seconds"
+
+    def test_run_grid_parallel_test_assert_response_time_search(self):
+        if self.browser.grid_enabled and not self.browser.serial_enabled:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.browser.browser_types)) as executor:
+                executor.map(self.test_assert_response_time_search, self.browser.browser_types)
+        else:
+            self.test_assert_response_time_search(self.browser.default_browser.lower())
+
+    def test_run_grid_parallel_test_assert_response_time_load_website(self):
+        if self.browser.grid_enabled and not self.browser.serial_enabled:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.browser.browser_types)) as executor:
+                executor.map(self.test_assert_response_time_load_website, self.browser.browser_types)
+        else:
+            self.test_assert_response_time_load_website(self.browser.default_browser.lower())
